ee88ce656a96b120402c165cd09e92e8ef18f921
unknown <tsvetoslavvasev@gmail.com>
2020-03-24 19:54:09 +0200

    Player now movew with wasd keys

--- Homework 1/Assets/script/PlayerMovement.cs
+++ Homework 1/Assets/script/PlayerMovement.cs
@@ -17,12 +17,37 @@ public class PlayerMovement : MonoBehaviour
         GetComponentInParent<BoxCollider>();
         FindObjectOfType<BoxCollider>();
     }
+
     void Update()
     {
+        Vector3 moveDirection = transform.position;
+        if (Input.GetKey("w"))
+        {
+            moveDirection.x += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            moveDirection.x -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            moveDirection.z += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            moveDirection.z -= speed * Time.deltaTime;
+        }
 
-        Vector3 moveDirection = new Vector3(1, 0, 0);
 
-        transform.position += moveDirection * speed * Time.deltaTime;
-        
+        transform.position = moveDirection;// * speed * Time.deltaTime;
+
     }
+    //void Update()
+    //{
+
+    //    Vector3 moveDirection = new Vector3(1, 0, 0);
+
+    //    transform.position += moveDirection * speed * Time.deltaTime;
+
+    //}
 }
