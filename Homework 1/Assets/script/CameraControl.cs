70b39345e4000ba1c72dc1cc091f028e30f8a113
unknown <tsvetoslavvasev@gmail.com>
2020-03-24 18:22:48 +0200

    added c# script for the first asignments

---
+++ Homework 1/Assets/script/CameraControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private GameObject player;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("MyPlayer");
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Homework 1/Assets/script/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 100;
+
+    [SerializeField]
+    private BoxCollider boxColider;
+
+    private void Start()
+    {
+        boxColider = GetComponent<BoxCollider>();
+        GetComponentInChildren<BoxCollider>();
+        GetComponentInParent<BoxCollider>();
+        FindObjectOfType<BoxCollider>();
+    }
+    void Update()
+    {
+
+        Vector3 moveDirection = new Vector3(1, 0, 0);
+
+        transform.position += moveDirection * speed * Time.deltaTime;
+        
+    }
+}
