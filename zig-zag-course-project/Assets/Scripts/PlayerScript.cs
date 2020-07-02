43853717591cd3ae9d8920770fe0418a47c84869
unknown <tsvetoslavvasev@gmail.com>
2020-07-02 15:20:58 +0300

    Course project for the game development course

---
+++ zig-zag-course-project/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float speed;
+
+    private Vector3 dir;
+
+    private bool isDead;
+
+    public GameObject particleSystem;
+    public GameObject resetButton;
+
+    private int score = 0;
+    public Text scoreText;
+
+    public Animator gameOverAnimator;
+
+    public Text newHighScore;
+
+    public Image background;
+
+    public Text[] scoreTexts;
+
+    public LayerMask whatIsGround;
+    private bool isPlaying = false;
+    public Transform contactPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isDead = false;
+        dir = Vector3.zero; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!IsGrounded() && isPlaying)
+        {
+            isDead = true;
+
+            GameOver();
+
+            resetButton.SetActive(true);
+
+            if (transform.childCount > 0)
+            {
+                transform.GetChild(0).transform.parent = null;
+            }
+        }
+        if (Input.GetMouseButtonDown(0) && !isDead)
+        {
+            isPlaying = true;
+            score++;
+            scoreText.text = score.ToString();
+            if(dir==Vector3.forward)
+            {
+                dir = Vector3.left;
+            }
+            else
+            {
+                dir = Vector3.forward;
+            }
+        }
+
+        float amountToMove = speed * Time.deltaTime;
+
+        transform.Translate(dir * amountToMove);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Pickup")
+        {
+            other.gameObject.SetActive(false);
+            Instantiate(particleSystem, transform.position, Quaternion.identity);
+            score += 3;
+            scoreText.text = score.ToString();
+        }
+    }
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    if(other.tag == "Tile")
+    //    {
+    //        RaycastHit hit;
+    //        Ray downRay = new Ray(transform.position, -Vector3.up); 
+    //        if(!Physics.Raycast(downRay,out hit))
+    //        {
+    //            isDead = true;
+    //            GameOver();
+    //            resetButton.SetActive(true);
+    //            if (transform.childCount > 0)
+    //            {
+    //                transform.GetChild(0).transform.parent = null;
+    //            }
+    //        }
+    //    }
+    //}
+
+    private void GameOver()
+    {
+        gameOverAnimator.SetTrigger("GameOver");
+        scoreTexts[1].text = score.ToString();
+        int bestScore = PlayerPrefs.GetInt("BestScore",0);
+
+        if(score > bestScore)
+        {
+            PlayerPrefs.SetInt("BestScore", score);
+            newHighScore.gameObject.SetActive(true);
+            background.color = new Color32(243, 77, 234,255);
+            foreach (Text txt in scoreTexts)
+            {
+                txt.color = Color.white;
+            }
+        }
+        scoreTexts[3].text = PlayerPrefs.GetInt("BestScore", 0).ToString();
+    }
+
+    private bool IsGrounded()
+    {
+        Collider[] colliders = Physics.OverlapSphere(contactPoint.position, .5f, whatIsGround);
+
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if(colliders[i].gameObject != gameObject)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ zig-zag-course-project/Assets/Scripts/TileManager.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+
+    public GameObject[] tilePrefabs;
+
+    public GameObject currentTile;
+
+    private static TileManager instance;
+
+    private Stack<GameObject> leftTiles = new Stack<GameObject>();
+    private Stack<GameObject> topTiles = new Stack<GameObject>();
+
+    public Stack<GameObject> LeftTiles { get => leftTiles; set => leftTiles = value; }
+    public Stack<GameObject> TopTiles { get => topTiles; set => topTiles = value; }
+
+    public static TileManager Instance 
+    {
+        get {
+            if(instance == null)
+            {
+                instance = GameObject.FindObjectOfType<TileManager>();
+            }
+            return instance; 
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        createTiles(50);
+        for (int i = 0; i < 100; i++)
+        {
+            SpawnTile();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void createTiles(int amount)
+    {
+        for(int i = 0; i < amount; i++)
+        {
+            LeftTiles.Push(Instantiate(tilePrefabs[0]));
+            TopTiles.Push(Instantiate(tilePrefabs[1]));
+            TopTiles.Peek().name = "TopTile";
+            TopTiles.Peek().SetActive(false);
+            LeftTiles.Peek().name = "LeftTile";
+            LeftTiles.Peek().SetActive(false);
+        }
+    }
+
+    public void SpawnTile()
+    {
+        if(LeftTiles.Count == 0 || TopTiles.Count == 0)
+        {
+            createTiles(10);
+        }
+        int randomIndex = Random.Range(0, 2);
+
+        if(randomIndex == 0)
+        {
+            GameObject tmp = LeftTiles.Pop();
+            tmp.SetActive(true);
+            tmp.transform.position = currentTile.transform.GetChild(0).transform.GetChild(randomIndex).position;
+            currentTile = tmp;
+        }
+        else if(randomIndex == 1)
+        {
+            GameObject tmp = TopTiles.Pop();
+            tmp.SetActive(true);
+            tmp.transform.position = currentTile.transform.GetChild(0).transform.GetChild(randomIndex).position;
+            currentTile = tmp;
+        }
+
+        int spawnPickup = Random.Range(0, 10);
+        if(spawnPickup == 0)
+        {
+            currentTile.transform.GetChild(1).gameObject.SetActive(true);
+        }
+        //currentTile = (GameObject)Instantiate(tilePrefabs[randomIndex], currentTile.transform.GetChild(0).transform.GetChild(randomIndex).position, Quaternion.identity);
+    }
+
+    public void resetGame()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
